2ecb586863377000829189f9e0c866cde4e72b9d
Michał Ostrowski <ostrowski.michal@gmail.com>
2014-01-25 10:11:52 +0100

    module

---
+++ lib/orientdb_binary.rb
@@ -0,0 +1,38 @@
+require 'socket'
+require 'bindata'
+
+require "orientdb_binary/config"
+
+require 'orientdb_binary/serialization/deserialize'
+
+require 'orientdb_binary/operation_types'
+require 'orientdb_binary/protocols/base'
+require 'orientdb_binary/protocols/bindata_primitives'
+require 'orientdb_binary/protocols/errors'
+
+require 'orientdb_binary/connection'
+require 'orientdb_binary/base'
+
+# server
+require 'orientdb_binary/protocols/connect'
+require 'orientdb_binary/protocols/db_exist'
+require 'orientdb_binary/protocols/db_create'
+require 'orientdb_binary/protocols/db_drop'
+require 'orientdb_binary/protocols/shutdown'
+require 'orientdb_binary/protocols/config_list'
+require 'orientdb_binary/protocols/config_get'
+require 'orientdb_binary/protocols/config_set'
+
+#database
+require 'orientdb_binary/protocols/db_close'
+require 'orientdb_binary/protocols/db_open'
+require 'orientdb_binary/protocols/db_reload'
+require 'orientdb_binary/protocols/record_load'
+require 'orientdb_binary/protocols/command'
+
+require 'orientdb_binary/server'
+require 'orientdb_binary/database'
+
+module OrientdbBinary
+
+end
