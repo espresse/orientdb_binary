1b08387f2ebf6106d47ec33887056702b4d1ac82
Michał Ostrowski <ostrowski.michal@gmail.com>
2014-01-25 10:05:53 +0100

    base

---
+++ lib/orientdb_binary/base.rb
@@ -0,0 +1,32 @@
+module OrientdbBinary
+  class OrientdbBase
+    attr_accessor :socket, :protocol, :session
+
+    def initialize(options)
+      defaults = {
+        host: 'localhost',
+        port: 2424
+      }
+
+      @options = defaults.merge(options)
+
+      @server_connection = OrientdbBinary::Connection.new(@options)
+      @socket = @server_connection.socket
+
+      @connected = false
+    end
+
+    def connected?
+      @connected
+    end
+
+    def disconnect
+      close
+      @server_connection.close()
+    end
+
+    def close
+    end
+
+  end
+end
---
+++ lib/orientdb_binary/config.rb
@@ -0,0 +1,5 @@
+module OrientdbBinary
+  VERSION = "0.5.0"
+  NAME = "OrientDB Binary Protocol Ruby Client"
+  PROTOCOL_VERSION = 19
+end
