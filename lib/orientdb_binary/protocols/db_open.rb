1e9999629f5c4ad1a94c4ccb605ce4411468dbdb
Michał Ostrowski <ostrowski.michal@gmail.com>
2014-01-25 21:34:14 +0100

    add missing fields

--- lib/orientdb_binary/protocols/db_open.rb
+++ lib/orientdb_binary/protocols/db_open.rb
@@ -31,6 +31,13 @@ module OrientdbBinary
         protocol_string :cluster_type
         int16 :cluster_data_segment_id
       end
+      int32 :cluster_config_bytes
+      skip length: :cluster_config_bytes, onlyif: :has_cluster_config?
+      protocol_string :orientdb_release
+
+      def has_cluster_config?
+        cluster_config_bytes > -1
+      end
     end
   end
 end
