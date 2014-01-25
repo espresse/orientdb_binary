68dbdc10aa48a8e4cc376f0ce3698c1358d2973b
Michał Ostrowski <ostrowski.michal@gmail.com>
2014-01-25 11:20:36 +0100

    fix

--- lib/orientdb_binary/protocols/db_countrecords.rb
+++ lib/orientdb_binary/protocols/db_countrecords.rb
@@ -6,14 +6,14 @@ module OrientdbBinary
       
       endian :big
 
-      int8 :operation, value: OrientdbBinary::OperationTypes::REQUEST_DB_SIZE
+      int8 :operation, value: OrientdbBinary::OperationTypes::REQUEST_DB_COUNTRECORDS
       int32 :session
     end
 
     class DbCountRecordsAnswer < BinData::Record
       endian :big
 
-      long :count_records
+      int64 :count_records
     end
   end
 end
